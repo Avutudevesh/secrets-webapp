d5379967f04e25965a8e0a3319304a8cecd888d0
Devesh Avutu <devesh14may@gmail.com>
2020-03-24 17:01:36 +0530

    installed required node modules

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function() {
+	console.log("Server Started on post 3000");
+});
