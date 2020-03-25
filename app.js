91eff82050e39c676edb2745ce6009dede1ab9cb
Devesh Avutu <devesh14may@gmail.com>
2020-03-25 17:34:45 +0530

    moved session secret into env file

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
 	session({
-		secret: "ghjgsadfsdfhdshgkhdsbtyjty",
+		secret: process.env.SESSION_SECRET,
 		resave: false,
 		saveUninitialized: false
 	})
