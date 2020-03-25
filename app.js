b8322c7b90361d632431620a24f0943e3c30a917
Devesh Avutu <devesh14may@gmail.com>
2020-03-25 13:42:51 +0530

    implemented local passport strategy for authentication

--- app.js
+++ app.js
@@ -3,23 +3,41 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	session({
+		secret: "ghjgsadfsdfhdshgkhdsbtyjty",
+		resave: false,
+		saveUninitialized: false
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
 
+mongoose.set("useCreateIndex", true);
+
 const userSchema = new mongoose.Schema({
 	email: String,
 	password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
 	res.render("home");
@@ -30,21 +48,17 @@ app.get("/login", function(req, res) {
 });
 
 app.post("/login", function(req, res) {
-	User.findOne({ email: req.body.username }, function(err, user) {
+	const user = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
+	req.login(user, function(err) {
 		if (err) {
-			res.send(err);
+			console.log(err);
 		} else {
-			if (user) {
-				bcrypt.compare(req.body.password, user.password, function(err, result) {
-					if (result === true) {
-						res.render("secrets");
-					} else {
-						res.render("home");
-					}
-				});
-			} else {
-				res.render("home");
-			}
+			passport.authenticate("local")(req, res, function() {
+				res.redirect("/secrets");
+			});
 		}
 	});
 });
@@ -53,19 +67,31 @@ app.get("/register", function(req, res) {
 	res.render("register");
 });
 
+app.get("/logout", function(req, res) {
+	req.logout();
+	res.redirect("/");
+});
+
+app.get("/secrets", function(req, res) {
+	if (req.isAuthenticated()) {
+		res.render("secrets");
+	} else {
+		res.redirect("/login");
+	}
+});
+
 app.post("/register", function(req, res) {
-	bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-		const newUser = new User({
-			email: req.body.username,
-			password: hash
-		});
-		newUser.save(function(err) {
-			if (err) {
-				console.log(err);
-			} else {
-				res.render("secrets");
-			}
-		});
+	User.register({ username: req.body.username }, req.body.password, function(
+		err,
+		user
+	) {
+		if (err) {
+			console.log(err);
+		} else {
+			passport.authenticate("local")(req, res, function() {
+				res.redirect("/secrets");
+			});
+		}
 	});
 });
 
